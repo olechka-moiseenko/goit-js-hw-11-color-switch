3023a12db6ab88f1f7db133dddcb2f34ee4388d9
olechka.moiseenko <olechka.moiseenko@gmail.com>
2021-05-13 11:19:34 +0300

    Добавлены активные и неактивные кнопки

--- src/colors-switcher.js
+++ src/colors-switcher.js
@@ -1,26 +1,34 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-  const btnStart = document.querySelector('button[data-action="start"]');
-  const btnStop = document.querySelector('button[data-action="stop"]');
-  const body = document.querySelector('body');
-  let timerId = null;
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+const btnStart = document.querySelector('button[data-action="start"]');
+const btnStop = document.querySelector('button[data-action="stop"]');
+const body = document.querySelector("body");
+let timerId = null;
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-  btnStart.addEventListener('click', () => {
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+// btnStop.disabled = true;
+btnStart.addEventListener("click", () => {
+  if (timerId === null) {
     timerId = setInterval(() => {
-      body.style['background-color'] = colors[randomIntegerFromInterval(0, colors.length - 1)];
-    },1000)
-  });
+      body.style["background-color"] =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+  }
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
 
- btnStop.addEventListener('click', () => {
-     clearInterval(timerId);
- });
+btnStop.addEventListener("click", () => {
+  clearInterval(timerId);
+  timerId = null;
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+});
